ae26a30ef9012b2e8a1a34fd5e9a70f875013ca8
ElieMirza <elie.mirza1987@gmail.com>
2022-01-22 13:19:15 +0200

    added score var and logic

--- game.js
+++ game.js
@@ -1,7 +1,9 @@
 window.onload = function() {
   var game_started = false;
+  var score = 0;
+  
   var boundary = document.getElementsByClassName("boundary");
-  document.getElementById("start").addEventListener("mouseover", function() {
+  document.getElementById("start").addEventListener("click", function() {
     game_started = true;
     console.log("started");
   });
@@ -12,13 +14,29 @@ window.onload = function() {
           event.target.classList.add("youlose");
           document.getElementById("status").textContent = "You lose!";
           game_started = false;
+          score = score - 10;
         }
       });
   }
   for(var i = 0; i < boundary.length; i++) {
       boundary[i].addEventListener("mouseleave", function(event) {
         event.target.classList.remove("youlose");
-        document.getElementById("status").textContent = 'Begin by moving your mouse over the "S".';
+        document.getElementById("status").textContent = 'Begin by clicking the "S".';
       });
 };
+  document.getElementById("end").addEventListener("mouseover", function(){
+    if(game_started) {
+      document.getElementById("status").textContent = "You win!";
+      game_started = false;
+      score = score + 5;
+    }
+  })
+
+
+
+
+
+
+
+
 }
