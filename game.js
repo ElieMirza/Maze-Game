0d131bfab4015774cd0c6a3a95cc2002c74ee64c
ElieMirza <elie.mirza1987@gmail.com>
2022-01-22 11:22:18 +0200

    added mouseover statement

--- game.js
+++ game.js
@@ -1,3 +1,10 @@
 window.onload = function() {
-  
+  var game_started = false;
+  var entered_maze = false;
+  var boundary = document.getElementsByClassName("boundary");
+  document.getElementById("start").addEventListener("mouseover", function() {
+    game_started = true;
+    entered_maze = true;
+    console.log("started");
+  })
 }
