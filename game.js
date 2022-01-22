47921d549efcca9c377098c1f6392d9b88128abc
ElieMirza <elie.mirza1987@gmail.com>
2022-01-22 12:40:37 +0200

    added youlose class when mouse hits a wall

--- game.js
+++ game.js
@@ -1,10 +1,18 @@
 window.onload = function() {
   var game_started = false;
-  var entered_maze = false;
   var boundary = document.getElementsByClassName("boundary");
   document.getElementById("start").addEventListener("mouseover", function() {
     game_started = true;
-    entered_maze = true;
     console.log("started");
-  })
-}
+  });
+
+  for(var i = 0; i < boundary.length; i++) {
+      boundary[i].addEventListener("mouseover", function(event) {
+        if(game_started) {
+          event.target.classList.add("youlose");
+          document.getElementById("status").textContent = "You lose!";
+          game_started = false;
+        }
+      });
+    }
+  }
