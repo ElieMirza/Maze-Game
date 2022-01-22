f45a308f156adbb345ddaca800684923187752cd
ElieMirza <elie.mirza1987@gmail.com>
2022-01-22 16:15:20 +0200

    working and cleaner

--- game.js
+++ game.js
@@ -1,82 +1,57 @@
 window.onload = function() {
+
   var game_started = false;
   var score = 0;
-
-  var score_text = document.getElementsByClassName("boundary example")[0];
-  score_text.innerHTML = "Score: 0";
-
   var reset_button = document.createElement("button");
-  reset_button.innerHTML = "Reset Score";
-
-  var score_div = document.getElementsByClassName("example")[0];
-  score_div.appendChild(reset_button);
+  var score_div = document.getElementsByClassName("boundary example")[0];
+  showScore();
 
   reset_button.addEventListener ("click", function() {
   score = 0;
+  showScore();
   });
 
-  var boundary = document.getElementsByClassName("boundary");
   document.getElementById("start").addEventListener("click", function() {
     document.getElementById("status").textContent = 'GO!';
     game_started = true;
-    });
+  });
 
-
+  var boundary = document.getElementsByClassName("boundary");
 
   for(var i = 0; i < boundary.length; i++) {
-      boundary[i].addEventListener("mouseover", function(event) {
-        if(game_started) {
-          event.target.classList.add("youlose");
-          document.getElementById("status").textContent = "You lose!";
-          game_started = false;
-          score = score - 10;
-          score_text.innerHTML = `Score: ${score}`;
+    boundary[i].addEventListener("mouseover", function(event) {
+      if(game_started) {
+        event.target.classList.add("youlose");
+        document.getElementById("status").textContent = "You lose!";
+        game_started = false;
+        score = score - 10;
+        showScore();
 
-          var reset_button = document.createElement("button");
-          reset_button.innerHTML = "Reset Score";
-
-          var score_div = document.getElementsByClassName("example")[0];
-          score_div.appendChild(reset_button);
-
-          reset_button.addEventListener ("click", function() {
-            document.getElementById("status").textContent = 'Begin by clicking the "S".';
-          score_text.innerHTML = "Score: 0";
-          score = 0;
-          });
-        }
-      });
+      }
+    });
   }
+
   for(var i = 0; i < boundary.length; i++) {
       boundary[i].addEventListener("mouseleave", function(event) {
         event.target.classList.remove("youlose");
       });
-};
+  };
   document.getElementById("end").addEventListener("mouseover", function(){
     if(game_started) {
       document.getElementById("status").textContent = "You win!";
       game_started = false;
       score = score + 5;
-      score_text.innerHTML = `Score: ${score}`;
+      showScore();
 
-      var reset_button = document.createElement("button");
-      reset_button.innerHTML = "Reset Score";
-
-      var score_div = document.getElementsByClassName("example")[0];
-      score_div.appendChild(reset_button);
-
-      reset_button.addEventListener ("click", function() {
-      document.getElementById("status").textContent = 'Begin by clicking the "S".';
-      score_text.innerHTML = "Score: 0";
-      score = 0;
-      });
     }
   })
 
+  function showScore() {
+  score_div.innerHTML = `Score: ${score}`;
+  reset_button.innerHTML = "Reset Score";
+  score_div.appendChild(reset_button);
+  };
 
-
-
-
-
-
+  
 
 }
